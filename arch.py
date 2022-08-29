73fad505a6a6439b35c956ffd0c8a4f829de3cfa
Frederik Pustelnik <fpustelnik@web.de>
2022-08-29 20:30:00 +0200

    Initial support for x86 & RISC-V

--- arch.py
+++ arch.py
@@ -14,43 +14,7 @@ class ArchitectureFunctionsBase:
     def get_reg_size(self, reg_name):
         pass
 
-    def handle_reg_ptr(self, reg_name, reg_to_alloca):
-        pass
-
-    def handle_reg_assign(self, reg_name, value, reg_to_alloca):
-        pass
-
-    def handle_reg_load(self, reg_name, reg_to_alloca):
-        pass
-
-    def get_return_register(self):
-        pass
-
-    def get_arg_registers(self, count, reg_to_alloca):
-        pass
-
-class ARMFunctions(ArchitectureFunctionsBase):
-
-    def __init__(self, builder):
-        self.builder = builder
-
-    def check_is_partial_reg(self, reg_name):
-        if reg_name[0] == "w":
-            return True
-        return False
-
-    def get_full_reg(self, reg_name):
-        return "x" + reg_name[1:]
-
-    def get_reg_size(self, reg_name):
-        if reg_name[0] == "x":
-            return 8
-        if reg_name[0] == "w":
-            return 4
-        else:
-            return 8
-
-    # Casts a register pointer to the needed value.
+     # Casts a register pointer to the needed value.
     def handle_reg_ptr(self, reg_name, reg_to_alloca):
         if self.check_is_partial_reg(reg_name):
             full_reg = self.get_full_reg(reg_name)
@@ -93,6 +57,33 @@ class ARMFunctions(ArchitectureFunctionsBase):
             return loaded_reg
 
     def get_return_register(self):
+        pass
+
+    def get_arg_registers(self, count, reg_to_alloca):
+        pass
+
+class ARMFunctions(ArchitectureFunctionsBase):
+
+    def __init__(self, builder):
+        self.builder = builder
+
+    def check_is_partial_reg(self, reg_name):
+        if reg_name[0] == "w":
+            return True
+        return False
+
+    def get_full_reg(self, reg_name):
+        return "x" + reg_name[1:]
+
+    def get_reg_size(self, reg_name):
+        if reg_name[0] == "x":
+            return 8
+        if reg_name[0] == "w":
+            return 4
+        else:
+            return 8
+
+    def get_return_register(self):
         return "x0"
 
     def get_arg_registers(self, count, reg_to_alloca):
@@ -114,6 +105,82 @@ class x86Functions(ArchitectureFunctionsBase):
     def __init__(self, builder):
         self.builder = builder
 
+    def check_is_partial_reg(self, reg_name):
+        # Registers like al, cl, dl, bl
+        if len(reg_name) == 2:
+            return True
+
+        # eax, ebx, ..., r8w, r9w, ...
+        if len(reg_name) == 3:
+            if reg_name[0] == "e" or reg_name[2] == "w":
+                return True
+
+        if len(reg_name) == 4:
+            if reg_name[3] == "w":
+                return True
+
+        return False
+
+    def get_full_reg(self, reg_name):
+        if len(reg_name) == 2:
+            return "r" + reg_name[0] + "x"
+
+        if len(reg_name) == 3:
+            if reg_name[0] == "e":
+                return "r" + reg_name[1:]
+
+            if reg_name[2] == "w":
+                return reg_name[0:2]
+
+        if len(reg_name) == 4:
+            if reg_name[3] == "w":
+                return reg_name[0:3]
+
+    def get_return_register(self):
+        return "rax"
+    
+    def get_arg_registers(self, count, reg_to_alloca):
+        param_regs = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"]
+
+        if count == 0:
+            return []
+        elif count <= 6:
+            loaded_regs = []
+
+            for param_reg in param_regs[0:count]:
+                loaded_regs.append(self.handle_reg_load(param_reg, reg_to_alloca))
+
+            return loaded_regs
+        else:
+            raise Exception("Too many function arguments!")
+
 class RISCVFunctions(ArchitectureFunctionsBase):
     def __init__(self, builder):
         self.builder = builder
+
+    def check_is_partial_reg(self, reg_name):
+        return False
+
+    def get_full_reg(self, reg_name):
+        return reg_name
+
+    def get_reg_size(self, reg_name):
+        return 8
+
+    def get_return_register(self):
+        return "a0"
+
+    def get_arg_registers(self, count, reg_to_alloca):
+        param_regs = ["a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7"]
+
+        if count == 0:
+            return []
+        elif count <= 8:
+            loaded_regs = []
+
+            for param_reg in param_regs[0:count]:
+                loaded_regs.append(self.handle_reg_load(param_reg, reg_to_alloca))
+
+            return loaded_regs
+        else:
+            raise Exception("Too many function arguments!")
